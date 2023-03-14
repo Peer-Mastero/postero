4b7ea47119e497892313311b9452a9c2e317bda0
Francis Anderson <wherlberg198@gmail.comgit config --global user.name Your>
2023-03-14 21:08:39 +0000

    added login and signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html",
-  
-],
+  content: [
+    './pages/**/*.html'
+  ],
   theme: {
     extend: {},
   },
